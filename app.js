edda023afde157237f4975e43cb26d44130e9e8b
Jarrad Plunkett <jmplun@gmail.com>
2014-03-16 21:45:40 +1100

    using express 4.0

--- app.js
+++ app.js
@@ -7,36 +7,45 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  ,favicon = require('static-favicon')
+  ,bodyParser = require('body-parser')
+  ,cookieParser = require('cookie-parser')
+  ,expressSession = require('express-session')
+  ,errorHandler = require('errorhandler')
+  ;
 
 var app = express();
-var expiryTime = 86400000*30; // set expiry cached time to ba a day
+var router = express.Router();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-//app.use(express.bodyParser());
-app.use(express.urlencoded);
-app.use(express.json());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-  app.locals.pretty = true;
+app.use(favicon());
+//app.use(express.logger('dev'));
+app.use(bodyParser());
+//app.use(express.methodOverride());
+//app.use(app.router);
+router.use(function(req,res,next){
+   console.log('%s %s %s', req.method, req.url, req.path);
 });
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+   // configure stuff here
+   app.use(errorHandler());
+   app.locals.pretty = true;
+}
+
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('My Express server listening on port ' + app.get('port'));
