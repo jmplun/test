ffcfb9884d73519ed22a1ee7b4cc2e3b28867f86
Jarrad Plunkett <jmplun@gmail.com>
2014-03-16 12:56:15 +1100

    Init
    Initial Upload of  test repository

---
+++ app.js
@@ -0,0 +1,43 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+var expiryTime = 86400000*30; // set expiry cached time to ba a day
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+app.use(express.urlencoded);
+app.use(express.json());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+  app.locals.pretty = true;
+});
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('My Express server listening on port ' + app.get('port'));
+});
